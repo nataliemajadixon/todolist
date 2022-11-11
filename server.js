7fa25a235efa4152a5c2ce0104bd83a0ef7d8c05
natolay <ndixon@seattleu.edu>
2022-11-11 13:37:31 +0100

    created a css and html for the todo list

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const router = require('./router/router');
+const bodyParser = require('body-parser');
 const server = express();
 const PORT = process.env.PORT || 7777;
-const MONGO_PASS = process.env.DB_password;
+const MONGO_PASS = process.env.DB_PASSWORD;
 const DB_URL = `mongodb+srv://${MONGO_PASS}@todolist.7zxfx7v.mongodb.net/?retryWrites=true&w=majority`
 
-// const DB_URL = `mongodb+srv://${MONGO_PASS}@to-do-list.fu6kjhl.mongodb.net/?retryWrites=true&w=majority`;
 mongoose
     .connect(DB_URL, {
         useNewUrlParser: true, useUnifiedTopology: true,
@@ -20,6 +20,8 @@ server.use(express.json());
 server.use(express.urlencoded({extended: false}));
 server.use(router);
 
+
+
 server.listen(PORT, async () => {
-    console.log(`Server is up on port ${PORT}`)
+    console.log(`Server is up on port ${PORT}`);
 });
