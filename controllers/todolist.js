c015efe1674fc0fd8480248129c4d893292864bd
natolay <ndixon@seattleu.edu>
2022-11-11 10:58:52 +0100

    created a post route and connected it to router

--- controllers/todolist.js
+++ controllers/todolist.js
@@ -1,1 +1,18 @@
 const ToDoListSchema = require('../model/ToDoListSchema');
+
+const createToDo = (req, res) => {
+    const todo = new ToDoListSchema({
+        id: req.body.id,
+        title: req.body.title,
+        description: req.body.description,
+        isCompleted: req.body.false,
+    });
+    todo.save((err, todo) => {
+        if(err){
+            res.send(err);
+        }
+        res.json(todo);
+    });
+};
+
+module.exports = {createToDo}
--- router/router.js
+++ router/router.js
@@ -1,9 +1,13 @@
+const { createToDo } = require('../controllers/todolist');
+
 const router = require('express').Router();
 
 
 
 router.get("/", (req, res) => {
-    res.send("To-Do-List App")
+    res.send("To-Do List App")
 })
+
+router.post("/todos", createToDo);
 
 module.exports = router;
